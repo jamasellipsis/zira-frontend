d8dd5e05b232a822406ad87216f506a66694b5d7
andergcp <andergcp@hotmail.com>
2020-10-19 18:23:11 -0500

    improving Navbar and Footer design

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -35,7 +35,7 @@ function Register(props) {
     return (
         <>
         <div >
-            <Button className="nav-link ml-2 btnSignup" onClick={() => setOpenModal(true)}>Sign up</Button>
+            <Button className="nav-link mx-lg-2 mx-auto btnSignup mt-1" onClick={() => setOpenModal(true)}>Sign up</Button>
             <Modal show={openModal} onHide={() => setOpenModal(false)}>
             <Form className='p-5' onSubmit={submit}>
                 <div className='text-center' >
