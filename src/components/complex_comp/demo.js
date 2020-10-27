b47b0c5349682690000f82f4fa8acd5ac8cc5933
andergcp <andergcp@hotmail.com>
2020-10-26 23:43:10 -0500

    Add title to iFrame

--- src/components/complex_comp/demo.js
+++ src/components/complex_comp/demo.js
@@ -9,7 +9,7 @@ const Demo = () => {
                 <h1 className="light-text text-center py-4" style={{fontWeight:"bold"}}>Demo</h1>
             </div>
             <div class="embed-responsive embed-responsive-1by1 w-50 my-3 mx-auto">
-              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen title="Zira Demo"></iframe>
             </div>
           </div>
         </div>
